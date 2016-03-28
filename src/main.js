2b7d8ffe6dbe6a0efa425f2815d7d6a56f0a0f0d
Phil <birkal@outlook.com>
2016-03-27 19:54:51 -0700

    Implement Cycle.js Hello World

--- src/main.js
+++ src/main.js
@@ -1,1 +1,31 @@
-console.log('Hello World')
+import Cycle from '@cycle/core'
+import {makeDOMDriver, label, input, h1, hr, div} from '@cycle/dom'
+import {Observable} from 'rx'
+
+function main(sources) {
+  const inputEv$ = sources.DOM
+    .select('.field')
+    .events('input')
+  const name$ = inputEv$
+    .map(ev => ev.target.value)
+    .startWith('')
+
+  const sinks = {
+    DOM: name$.map(name =>
+      div([
+        label('Name: '),
+        input('.field', {type: 'text'}),
+        hr(),
+        h1(`Hello ${name}!`)
+      ])
+    )
+  }
+
+  return sinks
+}
+
+const drivers = {
+  DOM: makeDOMDriver('#app')
+}
+
+Cycle.run(main, drivers)
